cf8538d8cd7e1d7ab787b32e522dab1c4ce68dec
trymzet <mzawadzki@dyvenia.com>
2022-01-09 22:50:49 +0100

    ✨ Added GitHub as storage to "hello, world" flow

--- elt_workshop/prefect/hello_world_1.py
+++ elt_workshop/prefect/hello_world_1.py
@@ -1,4 +1,5 @@
 from prefect import Flow, task
+from prefect.storage import GitHub
 
 
 @task
@@ -6,7 +7,10 @@ def echo(text):
     return text
 
 
-with Flow(
-    "My first Prefect flow",
-) as flow:
+STORAGE = GitHub(
+    repo="dyvenia/elt_workshop", path="elt_workshop/prefect/hello_world_1.py"
+)
+
+
+with Flow("My first Prefect flow", storage=STORAGE) as flow:
     hello_world = echo("Hello, world!")
