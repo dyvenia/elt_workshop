4bb2482ff03d0d05a9e1625112f622b90892c3ac
trymzet <mzawadzki@dyvenia.com>
2022-01-10 00:14:29 +0100

    ✨ Added a change that allows user to use prefect cloud CLI in the notebook

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ if [ -f docker/.env ]; then
   export $(echo $(cat docker/.env | sed 's/#.*//g'| xargs) | envsubst)
 fi
 
-pip install --user -r local-requirements.txt
+pip install -r local-requirements.txt
 
 # Authenticate to DockerHub
 docker login -u=$DOCKERHUB_USER -p=$DOCKERHUB_TOKEN
@@ -12,4 +12,5 @@ docker build . -t prefect_with_jupyter -f docker/Dockerfile
 
 cd docker && \
   docker-compose up -d && \
+  docker exec elt_jupyter_lab sh -c "prefect auth login --key  $PREFECT_API_KEY" && \
   sh start_prefect_agent.sh
